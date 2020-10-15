6403cb44831ea616f10620294281b5c52d0754f8
Bagus_24 <Bagus_24@Bagus24>
2020-10-15 19:18:31 +0700

    new commit

---
+++ src/pboif2/pkg10119064/latihan2/deklarasivariabel/PBOIF210119064Latihan2DeklarasiVariabel.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119064.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author 
+ * NAMA         : Bagus Budi Wibowo
+ * KELAS        : PBOIF2 
+ * NIM          : 10119064 
+ * Deskripsi Program : Program ini berisi program untuk mendeklarasikan variabel
+ * dan pengaksesan variabel
+ */
+public class PBOIF210119064Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Deklarasi Variabel
+        int nilaiInt;
+        final double PHT = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarekter;
+
+        //Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarekter = 'D';
+        
+        //Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHT = " + PHT);
+        System.out.println("Isi variabel Logika = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarekter);
+    }
+    
+}
